256f9cbe5781f564f1035ec3c62581f8d006cc94
matt111302 <baker.mat@northeastern.edu>
2026-04-03 17:25:14 -0400

    Reset calibration values

--- robotcars/car_tools/params.py
+++ robotcars/car_tools/params.py
@@ -18,38 +18,36 @@ PARAMS = {
     },
     'squeaky': {
         'circle': {
-            'steering':15,
+            'steering':14,
             'forward_for': 10,
-            'speed':80
-        },
-        'square': {
-            'forward_for':3,
-            'backward_for': 1.5,
-            'speed':85
-        },
-        'hexagon': {
-            'forward_for':1.5,
-            'motor_speed':0,
-            'sleep':1,
-            'speed':80
-        }
-    },
-    'lucky': {
-        'circle': {
-            'steering':13,
-            'forward_for': 9,
-            'speed':80
+            'speed':40
         },
         'square': {
             'forward_for':3,
             'backward_for': 1.3,
-            'speed':80
+            'speed':40
         },
         'hexagon': {
-            'forward_for':1.5,
-            'motor_speed':0,
-            'sleep':0.8,
-            'speed':80
+            'forward_for':1,
+            'speed':40,
+            'turn_time':0.9
         }
+    },
+    'lucky': {
+        'circle': {
+            'steering':14,
+            'forward_for': 10,
+            'speed':40
+        },
+        'square': {
+            'forward_for':3,
+            'backward_for': 1.3,
+            'speed':40
+        },
+        'hexagon': {
+            'forward_for':1,
+            'speed':40,
+            'turn_time':0.9
+        } 
     }
 }
